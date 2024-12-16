b03b86f7058eb7805a05e0579b2f7843776e89b6
Temirlan <ltemirlan2003@gmail.com>
2024-12-17 01:21:42 +0600

    Configure to npm package

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/react-dynamic-fields"),
+      name: "react-dynamic-fields-kit",
+      fileName: (format) => `react-dynamic-fields-kit.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
